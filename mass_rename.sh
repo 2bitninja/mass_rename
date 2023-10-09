3a98af070bae4c420e81b1c393148840a6218b8c
2bitninja <2bitninja@gmail.com>
2023-10-10 06:53:25 +0900

    working

--- mass_rename.sh
+++ mass_rename.sh
@@ -3,11 +3,14 @@
 # Created on 20231001
 
 ### Basic Script
+echo -e "This script renames a group of files"
+read -p "Please input what you don't want: " remove
+read -p "Please input what you want to add: " add
 ls | while read line
 do echo $line
 # can use cp instead 
-mv "$line" "$(echo -e "$line" |sed -e 's///' -e 's///' )"
+mv "$line" "$(echo -e "$line" |sed -e 's/$remove/$add/' )"
 done
 
 # can use path 
-#mv "$line" "/Users/<username>/Dowloads/$(echo -e "$line" |sed -e 's///' -e 's///' )"
+#mv "$line" "/Users/<username>/Dowloads/$(echo -e "$line" |sed -e 's/$remove/$add/' -e 's///' )"
