becbd0e2ebb436aa5119e4aac5a272d1b0769be7
2Bit Ninja <2bitninja@gmail.com>
2023-10-12 18:01:04 +0900

    Added a eature to main script. Updated the list.txt. Created temp file

--- DummyFileCreater.sh
+++ DummyFileCreater.sh
@@ -2,7 +2,7 @@
 # Created to on 20231009 to make dummy files for testing the mass_rename script
 mkdir test-dir 2>/dev/null
 cd test-dir
-cat ../list.txt |while read -r line
-do echo -e  $line
+cat ../list.txt | while read -r line
+do echo -e $line
 touch "$line"
 done
--- mass_rename.sh
+++ mass_rename.sh
@@ -2,17 +2,28 @@
 # This script is for doing mass renamaning of media files.
 # Created on 20231001
 
-### Basic Script
-echo -e "This script renames a group of files"
-read -p "Where are the files located?: " FL
-cd $FL
-read -p "Please input what you don't want: " remove
-read -p "Please input what you want to add: " add
-ls | while read line
-do echo $line
-# can use cp instead 
-mv $FL/"$line" "$(echo -e "$FL/$line" |sed -e "s/$remove/$add/" )"
-done
+### VAR Section
+bold=$(tput bold)
+normal=$(tput sgr0)
 
-# can use path 
-#mv "$line" "/Users/<username>/Dowloads/$(echo -e "$line" |sed -e 's/$remove/$add/' -e 's///' )"
+### Basic Script
+clear
+echo -e "\tThis script renames a group of files. Pleas use with care."
+read -p "$bold Where are the files located?: $normal" FL
+#echo -e "\n$bold Are these the files you want to rename?$normal\n$(pwd)\n$(ls $FL)\n"
+echo -e "\n$bold These are the files located at:$normal $(cd $FL;pwd)\n$(ls $FL)\n"
+read -p "$bold Please input what you want to changed: $normal" remove
+ls $FL|grep "$remove"
+read -p "$bold Please input what you want to add: $normal" add
+
+ls $FL | while read line
+do #echo $line
+mv $FL/"$line" "$(echo -e "$FL/$line" |sed -e "s/$remove/$add/" )" 2>/dev/null
+done
+#clear
+echo -e "\n\t$bold The files have been renamed to:$normal"
+ls $FL
+
+### notes
+# To remove special [ ] 
+# sed -e 's/\[//' -e 's/\]//'
