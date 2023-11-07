01e63a67b15b4576d0d6f128fbbbd8f05ff3f0e0
2bitninja <2bitninja@gmail.com>
2023-11-07 21:52:13 +0900

    Added mass_rename.py

---
+++ mass_rename.py
@@ -0,0 +1,6 @@
+import os
+def rename_files(directory_path, old_name, new_name):
+for filename in os.listdir(directory_path):
+if old_name in filename:
+new_filename = filename.replace(old_name, new_name)
+os.rename(os.path.join(directory_path, filename), os.path.join(directory_path, new_filename))
