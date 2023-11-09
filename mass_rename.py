8b4d5288887587d98a7796f11742faa26da9f5a4
2Bit Ninja <2bitninja@gmail.com>
2023-11-09 17:00:08 +0900

    minor change

--- mass_rename.py
+++ mass_rename.py
@@ -1,5 +1,6 @@
 import os
 def rename_files(directory_path, old_name, new_name):
+
 for filename in os.listdir(directory_path):
 if old_name in filename:
 new_filename = filename.replace(old_name, new_name)
